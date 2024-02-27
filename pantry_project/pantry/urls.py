b92929473a951d51d8495f657a18f6c2cbd63a4b
Andrew Holligan <2771095h@student.gla.ac.uk>
2024-02-27 19:48:38 +0000

    feat: init settings.py + init urls.py + init index.html

---
+++ pantry_project/pantry/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from pantry import views
+
+app_name = "pantry"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- pantry_project/pantry/views.py
+++ pantry_project/pantry/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# TEMPLATE VIEWS
+
+
+def index(request):
+    return render(request, "pantry/index.html")
