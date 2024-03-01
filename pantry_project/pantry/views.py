b71e48b77b0e71db23ea748833302d6c36ddfbc8
Andrew Holligan <2771095h@student.gla.ac.uk>
2024-03-01 12:24:40 +0000

    feat: recipes pages css + recipes html using context dictionary

--- pantry_project/pantry/views.py
+++ pantry_project/pantry/views.py
@@ -12,4 +12,19 @@ def about(request):
 
 
 def recipes(request):
-    return render(request, "pantry/recipes.html")
+    # TESTING PURPOSE
+    recipes = [
+        {
+            "name": "Spaghetti Bolegnaise",
+            "link": "",
+            "image": "",
+            "rating": 4.67,
+            "saves": 34,
+            "difficulty": "beginner",
+            "cuisine": "Italian",
+            "prep": "1:30",
+            "cook": "0:30",
+        }
+    ] * 20
+    context_dict = {"recipes": recipes}
+    return render(request, "pantry/recipes.html", context=context_dict)
