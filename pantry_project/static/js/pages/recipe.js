c8c092724c9807a4a700ef137c9f1d29af63af0e
Andrew Holligan <2771095h@student.gla.ac.uk>
2024-03-02 20:07:51 +0000

    feat: stars + bookmark interactivity

--- pantry_project/static/js/pages/recipe.js
+++ pantry_project/static/js/pages/recipe.js
@@ -1,19 +1,40 @@
 // GLOBALS
 
+const STARS_AMOUNT = 5;
+
 let rating = 0;
+let bookmarked = false;
 
 // STAR RATING
+
+function updateStars(index) {
+  let stars = document.getElementsByClassName("star");
+
+  rating = index;
+
+  for (let i = 0; i < STARS_AMOUNT; i++) {
+    if (i < index) {
+      stars[i].style.backgroundImage = "url(../../static/images/star.svg)";
+    } else {
+      stars[i].style.backgroundImage =
+        "url(../../static/images/star-empty.svg)";
+    }
+  }
+}
 
 function initStars() {
   let container = document.getElementById("stars");
 
-  // 5 stars
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < STARS_AMOUNT; i++) {
     let star = document.createElement("button");
 
     star.id = "star-" + (i + 1);
     star.style.backgroundImage = "url(../../static/images/star-empty.svg)";
     star.className = "star";
+
+    star.onclick = () => {
+      updateStars(i + 1);
+    };
 
     container.appendChild(star);
   }
@@ -21,10 +42,31 @@ function initStars() {
 
 // BOOKMARK
 
+function updateBookmark() {
+  let bookmark = document.getElementById("bookmark");
+
+  // user deselecting bookmark
+  if (bookmarked) {
+    bookmarked = false;
+    bookmark.style.backgroundImage =
+      "url(../../static/images/bookmark-empty.svg)";
+    return;
+  }
+
+  // user selecting bookmark
+  bookmarked = true;
+  bookmark.style.backgroundImage = "url(../../static/images/bookmark.svg)";
+}
+
 function initBookmark() {
   let bookmark = document.getElementById("bookmark");
+
   bookmark.style.backgroundImage =
     "url(../../static/images/bookmark-empty.svg)";
+
+  bookmark.onclick = () => {
+    updateBookmark();
+  };
 }
 
 initStars();
