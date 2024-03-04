9fd6f4d2178590b792372d3ebbfb3bd72b339bc7
Andrew McGill <2762627m@student.gla.ac.uk>
2024-03-04 10:13:23 +0000

    started recipe model

--- pantry_project/pantry/models.py
+++ pantry_project/pantry/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+def Recipe(models.Model):
+    title = models.CharField(max_length = 200)
+    photo_url = models.CharField(max_length = 500)
+    desc = models.CharField(max_length = 500)
+    ingredients = models.CharField(max_length = 2000)
+    steps = models.Char
+    
+    
+    def __str__(self):
+        return self.title
