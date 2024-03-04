c8cf79c6e5778b7c2f8852b4ad2a4e469bc67137
Andrew McGill <2762627m@student.gla.ac.uk>
2024-03-04 10:53:25 +0000

    user model

--- pantry_project/pantry/models.py
+++ pantry_project/pantry/models.py
@@ -4,11 +4,25 @@ from django.db import models
 
 def Recipe(models.Model):
     title = models.CharField(max_length = 200)
-    photo_url = models.CharField(max_length = 500)
+    photo_url = models.URLField(max_length = 500)
     desc = models.CharField(max_length = 500)
     ingredients = models.CharField(max_length = 2000)
-    steps = models.Char
+    steps = models.CharField(max_length = 10_000)
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    difficulty = modelsIntegerField()
+    star_rating = models.IntegerField()
+    no_of_saves = models.IntegerField()
+    date_pub = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     
     def __str__(self):
         return self.title
+    
+
+    
+def User(models.Model):
+    likes = models.IntegerField()
+    date_pub = models.DateField()
+    
