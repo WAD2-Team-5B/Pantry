d5fb1ecc71531b6c1d290cc40030957a96172520
Nicole Sung <2521002s@student.gla.ac.uk>
2024-03-04 12:34:50 +0000

    all models done

--- pantry_project/pantry/models.py
+++ pantry_project/pantry/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 
 class Cuisine(models.Model):
@@ -7,7 +8,6 @@ class Cuisine(models.Model):
 
     def __str__(self):
         return self.type
-    
 
 class Category(models.Model):
     type = models.CharField(max_length=100, unique=True)
@@ -30,25 +30,23 @@ class Review(models.Model):
         unique_together = ('user', 'recipe')
 
 
-# may need to add model for saving recipe????
-
 class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
-    
+
     title = models.CharField(max_length = 200)
-    photo_url = models.URLField(max_length = 500)
+    photo = models.ImageField(upload_to='recipe_images')
     desc = models.CharField(max_length = 500)
     ingredients = models.CharField(max_length = 2000)
     steps = models.CharField(max_length = 10_000)
     prep_time = models.CharField(max_length=4)
     cook_time = models.CharField(max_length=4)
     difficulty = models.CharField(max_length=1)
-    star_rating = models.FloatField()
+    star_count = models.IntegerField()
+    star_submissions = models.IntegerField()
     no_of_saves = models.IntegerField()
     date_pub = models.DateField()
-    
     
     
     def __str__(self):
@@ -57,5 +55,15 @@ class Recipe(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
     
     
+class SavedRecipes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
