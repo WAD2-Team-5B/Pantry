6badea5900eccc2ecaccaa0fb70820e38177f200
Andrew McGill <2762627m@student.gla.ac.uk>
2024-03-10 14:16:26 +0000

    views progress - needs tested

--- pantry_project/pantry/urls.py
+++ pantry_project/pantry/urls.py
@@ -11,20 +11,10 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("signup/", views.signup, name="signup"),
     path("logout/", views.logout, name="logout"),
-    # TESTING PURPOSES
-    # TODO - CHANGE TO INCLUDE USER ID AND RECIPE ID
-    path("recipe/", views.recipe, name="recipe"),
+    path("recipes/<int:user_id>/<int:recipe_id>", views.recipe, name="recipe"),
     path("create-a-recipe/", views.create_a_recipe, name="create-a-recipe"),
-    # TESTING PURPOSE
-    # TODO - CHANGE TO USER ID
-    path("user-profile/user_id", views.user_profile, name="user-profile"),
-    # TESTING PURPOSE
-    # TODO - CHANGE TO USER ID
-    path("user-profile/user-recipes/", views.user_recipes, name="user-recipes"),
-    # TESTING PURPOSE
-    # TODO - CHANGE TO USER ID
-    path("user-profile/saved-recipes/", views.saved_recipes, name="saved-recipes"),
-    # TESTING PURPOSE
-    # TODO - CHANGE TO USER ID
-    path("user-profile/user-reviews/", views.user_reviews, name="user-reviews"),
+    path("<int:user_id>", views.user_profile, name="user-profile"),
+    path("<int:user_id>/user-recipes/", views.user_recipes, name="user-recipes"),
+    path("<int:user_id>/saved-recipes/", views.saved_recipes, name="saved-recipes"),
+    path("<int:user_id>/user-reviews/", views.user_reviews, name="user-reviews"),
 ]
