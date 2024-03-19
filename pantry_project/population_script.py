b858568536c5955034b284c15a105a1680d0f33b
Jeval Halton <jevocity04@gmail.com>
2024-03-19 17:22:44 +0000

    reduce print statements in population script

--- pantry_project/population_script.py
+++ pantry_project/population_script.py
@@ -133,7 +133,6 @@ def create_users_and_profiles():
         userprofile_object = add_userprofile(
             user_object, user_data["image"], user_data["bio"]
         )
-        print(f"Created User: {user_object.username}\tCreated UserProfile")
 
 
 def create_cuisines_and_categories():
@@ -336,7 +335,6 @@ def create_recipes():
 
     for recipe_data in recipes_data:
         recipe = add_recipe(recipe_data)
-        print(f"{recipe.title} has been created")
 
 
 def create_reviews():
@@ -376,7 +374,6 @@ def create_reviews():
     for review_data in reviews_data:
         add_review(review_data)
         username = review_data["user"]
-        print(f"created reviews by user: {username}")
 
 
 if __name__ == "__main__":
@@ -385,3 +382,4 @@ if __name__ == "__main__":
     create_cuisines_and_categories()
     create_recipes()
     create_reviews()
+    print("finished")
